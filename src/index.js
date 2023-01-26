96f9b3c81f6e6d12a28423bb67b097541844d292
daryapatsko <116173746+daryapatsko@users.noreply.github.com>
2023-01-26 13:42:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let num=Math.abs(n);
+    revnum=0;
+    while (num>0){
+      revnum*10+ num%10;
+      num=Math.floor(num/10)
+    }
+    return revnum;
 }
